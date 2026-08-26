c1df6b8eb9b4ab47a4495369ecc9df175cd344bb
9aggelos <9aggelosdel@gmail.com>
2026-08-26 16:27:58 +0300

    Update BASE_URL in sitemap route

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 
-const BASE_URL = "https://isla-vida-mykonos.lovable.app";
+const BASE_URL = "https://www.islavidajmk.com/";
 
 interface SitemapEntry {
   path: string;
